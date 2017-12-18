0317135881c1ba57269297f23f8cf7555628ecae
Sonny Li <sl4142@columbia.edu>
2017-12-18 11:54:01 -0500

    Week 2

---
+++ Programs/youngmoney.c
@@ -0,0 +1,33 @@
+// Sonny Li
+// Intro to Programming in C
+
+#include <stdio.h>
+
+int main() {
+    
+    int a = 0;
+
+    while (a < 4)
+    {
+        printf("a milli\n");
+        a++;
+    }
+
+    printf("a mi\n");
+    printf("a mi\n");
+
+    // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+    int b;
+
+    for (b = 0; b < 4; b++)
+    {
+        printf("a milli\n");
+    }
+
+    printf("a mi\n");
+    printf("a mi\n");
+
+    return 0;
+
+}

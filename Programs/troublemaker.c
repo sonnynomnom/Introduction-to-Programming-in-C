400d3d3427b2662cd4891ae3a9f8fe760350eec2
Sonny Li <sl4142@columbia.edu>
2017-12-18 12:29:39 -0500

    Week 2

---
+++ Programs/troublemaker.c
@@ -0,0 +1,16 @@
+// Sonny Li
+// Intro to Programming in C
+
+#include <stdio.h>
+
+int main()
+{
+
+    for (int x = 0; x < 50; x++)
+    {
+
+        printf("I will not throw paper airplanes in class.\n");
+
+    }
+
+}

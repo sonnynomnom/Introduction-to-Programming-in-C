13c96d1f0d4ac685b7d612992fc636a2802c1cb9
Sonny Li <sl4142@columbia.edu>
2017-12-20 14:25:14 -0500

    Week 1

--- Programs/magic8ball.c
+++ Programs/magic8ball.c
@@ -14,79 +14,49 @@ int main() {
     printf("Do you have a question for me? Ask away!\n\n");
 
     if (choice == 1) {
-
         printf("MAGIC 8-BALL SAYS: #YAS\n");
-
     }
     else if (choice == 2) {
-
         printf("MAGIC 8-BALL SAYS: Yes, yes, and YES!\n");
-
     }
     else if (choice == 3) {
-
         printf("MAGIC 8-BALL SAYS: The stars align. It's a definite yes.\n");
-
     }
     else if (choice == 4) {
-
         printf("MAGIC 8-BALL SAYS: Okie doke from me~\n");
-
     }
     else if (choice == 5) {
-
         printf("MAGIC 8-BALL SAYS: Yeah, I think so.\n");
-
     }
     else if (choice == 6) {
-
         printf("MAGIC 8-BALL SAYS: Never in your life.\n");
-
     }
     else if (choice == 7) {
-
         printf("MAGIC 8-BALL SAYS: You need to get outta there ASAP!\n");
-
     }
     else if (choice == 8) {
-
         printf("MAGIC 8-BALL SAYS: I don't have a good feeling about this.\n");
-
     }
     else if (choice == 9) {
-
         printf("MAGIC 8-BALL SAYS: N A H\n");
-
     }
     else if (choice == 10) {
-
         printf("MAGIC 8-BALL SAYS: Reply hazy, check back later.\n");
-
     }
     else if (choice == 11) {
-
         printf("MAGIC 8-BALL SAYS: One day it shall all become clear.\n");
-
     }
     else if (choice == 12) {
-
         printf("MAGIC 8-BALL SAYS: You are not gonna like the answer.\n");
-
     }
     else if (choice == 13) {
-
         printf("MAGIC 8-BALL SAYS: Idk *_*\n");
-
     }
     else if (choice == 14) {
-
         printf("MAGIC 8-BALL SAYS: Maaaybe, try again!\n");
-
     }
     else {
-      
         printf("8-BALL ERROR!\n");
-
     }
 
     return 0;
